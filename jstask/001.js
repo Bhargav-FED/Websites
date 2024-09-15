040e09255b7833de49fef372cb7e56925e43bafd
Bhargav Adulapuram <bhargavadulapuram98@gmail.com>
2024-09-16 00:48:51 +0530

    save

--- jstask/001.js
+++ jstask/001.js
@@ -1,33 +1,41 @@
+// Arthematic operators
 let a = 20;
 let b = 10;
-let c = a+b;
+let c = a + b;
 console.log(c)
 
-var num1=10;
-console.log(num1)
-let num2 =20;
-console.log(num2)
-const num3=50;
-console.log(num3)
-const num4 = 100;
-console.log(num4)
+let x = 22;
+let y = 34;
+let z = x - y;
+console.log(z)
 
+let d = 22;
+let e = 34;
+let f = d * e;
+console.log(f)
 
+let d1 = 22;
+let e2 = 34;
+let f3 = d1 ** e2;
+console.log(f3)
 
-    var x=10;
-    var y=20;
-    var z=x+y;
-    console.log(z)
+let x1 = 22;
+let y2 = 34;
+let z3 = x1 / y2;
+console.log(z3)
 
+let d6 = 22;
+let e7 = 34;
+let f8 = d6 % e7;
+console.log(f8)
 
-    var x=100;
-    console.log(x)
-    var y=50;
-    console.log(y)
+let title = 'JavaScript wikipedia';
+title ||= 'accumenta';
+console.log(title);
 
-
-    const e=100;
-    const f=50;
-    const g=e-f;
-    console.log(g)
-    
+let person = {
+    firstName: 'html',
+    lastName: 'css',
+  };
+  person.lastName &&= 'react';
+  console.log(person); 
--- jstask/002.js
+++ jstask/002.js
@@ -1,33 +1,9 @@
-let a =20;
-let b=10;
-let c=a+b;
-console.log(c)
-
-var num1=10;
-console.log(num1)
-let num2 =20;
-console.log(num2)
-const num3=50;
-console.log(num3)
-const num4 = 100;
-console.log(num4)
+let sum = 0;
+// looping from i = 1 to number
+for (let i = 1; i <= 20; i++) {
+    sum += i;
+}
+console.log("The sum of the first 10 natural numbers is:", sum);
+// in each iteration, i is increased by 1
 
 
-
-    var x=10;
-    var y=20;
-    var z=x+y;
-    console.log(z)
-
-
-    var x=100;
-    console.log(x)
-    var y=50;
-    console.log(y)
-
-
-    const e=100;
-    const f=50;
-    const g=e-f;
-    console.log(g)
-    
