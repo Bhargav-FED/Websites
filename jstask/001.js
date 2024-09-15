630ef3be1a5c9089447c2d7e2af4e815f3e98c65
Bhargav Adulapuram <bhargavadulapuram98@gmail.com>
2024-09-15 22:56:22 +0530

    save

---
+++ jstask/001.js
@@ -0,0 +1,33 @@
+let a = 20;
+let b = 10;
+let c = a+b;
+console.log(c)
+
+var num1=10;
+console.log(num1)
+let num2 =20;
+console.log(num2)
+const num3=50;
+console.log(num3)
+const num4 = 100;
+console.log(num4)
+
+
+
+    var x=10;
+    var y=20;
+    var z=x+y;
+    console.log(z)
+
+
+    var x=100;
+    console.log(x)
+    var y=50;
+    console.log(y)
+
+
+    const e=100;
+    const f=50;
+    const g=e-f;
+    console.log(g)
+    
