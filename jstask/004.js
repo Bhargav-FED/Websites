77d7166c18a8deaa651190e02c093d8d96249b80
Bhargav Adulapuram <bhargavadulapuram98@gmail.com>
2024-09-17 12:40:03 +0530

    save

---
+++ jstask/004.js
@@ -0,0 +1,14 @@
+const person = {
+    name: "bhargav",
+    city: "hyderabad",
+    age: 24
+  };
+  let keys = Object.keys(person)
+  console.log(keys)
+  let values = Object.values(person)
+  console.log(values)
+  let entries = Object.entries(person)
+  console.log(entries)
+
+   
+   
